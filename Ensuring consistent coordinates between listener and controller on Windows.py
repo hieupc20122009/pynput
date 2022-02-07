c7921e9025d7ea7655f6c593b3699a08468e107b
hieupc20122009 <96122158+hieupc20122009@users.noreply.github.com>
2022-02-07 07:41:21 +0700

    Create Ensuring consistent coordinates between listener and controller on Windows.py

---
+++ Ensuring consistent coordinates between listener and controller on Windows.py
@@ -0,0 +1,10 @@
+Recent versions of _Windows_ support running legacy applications scaled when the system scaling has been increased beyond 100%. This allows old applications to scale, albeit with a blurry look, and avoids tiny, unusable user interfaces.
+This scaling is unfortunately inconsistently applied to a mouse listener and a controller: the listener will receive physical coordinates, but the controller has to work with scaled coordinates.
+This can be worked around by telling Windows that your application is DPI aware. This is a process global setting, so _pynput_ cannot do it automatically. Do enable DPI awareness, run the following code:
+
+import ctypes
+
+
+PROCESS_PER_MONITOR_DPI_AWARE = 2
+
+ctypes.windll.shcore.SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE)
